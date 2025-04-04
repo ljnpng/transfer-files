41ac460e3c70e74323c1ee25c15ffcc542949af1
gengliming <gengliming@meituan.com>
2025-04-04 21:57:51 +0800

    修复FileTransfer中的类型错误: 在usePeerConnection中暴露peer对象

--- src/hooks/usePeerConnection.ts
+++ src/hooks/usePeerConnection.ts
@@ -155,6 +155,7 @@ export default function usePeerConnection(options: PeerConnectionOptions = {}) {
     connectionStatus,
     connected,
     connectToPeer,
-    sendData
+    sendData,
+    peer: peerRef.current
   };
 } 
